3fd5715775bee873010593733de84c038495e1c6
CamilaLell <camiluciana03@gmail.com>
2025-03-14 12:42:52 -0300

    Update modulo1.py

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,3 +1,6 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
 print ("hola mundo)")
+
+
+print ("hola holaa")
